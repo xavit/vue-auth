42cf22110766c5f6c931bcfb90bcdc875f3ac5b8
Javier Claros <javiclaros@gmail.com>
2019-07-04 18:22:54 -0400

    some

--- src/router.js
+++ src/router.js
@@ -1,12 +1,17 @@
 import Vue from "vue";
 import Router from "vue-router";
+
+import store from "./store";
 import Home from "./views/Home.vue";
+import About from "./views/About.vue";
+import Login from "./components/Login.vue";
+import Secure from "./components/Secure.vue";
+import Register from "./components/Register.vue";
 
 Vue.use(Router);
 
-export default new Router({
+let router = new Router({
   mode: "history",
-  base: process.env.BASE_URL,
   routes: [
     {
       path: "/",
@@ -14,13 +19,41 @@ export default new Router({
       component: Home
     },
     {
+      path: "/login",
+      name: "login",
+      component: Login
+    },
+    {
+      path: "/register",
+      name: "register",
+      component: Register
+    },
+    {
+      path: "/secure",
+      name: "secure",
+      component: Secure,
+      meta: {
+        requiresAuth: true
+      }
+    },
+    {
       path: "/about",
       name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
+      component: About
     }
   ]
 });
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    if (store.getters.isLoggedIn) {
+      next();
+      return;
+    }
+    next("/login");
+  } else {
+    next();
+  }
+});
+
+export default router;
