b556618b0fc313915546ea005dfa54c252e020f9
Javier Claros <javiclaros@gmail.com>
2019-07-04 17:52:01 -0400

    first commit

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,28 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">About</router-link
+      ><span v-if="isLoggedIn"> | <a @click="logout">Logout</a></span>
     </div>
     <router-view />
   </div>
 </template>
-
+<script>
+export default {
+  computed: {
+    isLoggedIn: function() {
+      return this.$store.getters.isLoggedIn;
+    }
+  },
+  methods: {
+    logout: function() {
+      this.$store.dispatch("logout").then(() => {
+        this.$router.push("/login");
+      });
+    }
+  }
+};
+</script>
 <style>
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+//axios
+import store from "./store";
+import Axios from "axios";
+Vue.prototype.$http = Axios;
+const token = localStorage.getItem("token");
+if (token) {
+  Vue.prototype.$http.defaults.headers.common["Authorization"] = token;
+}
 
 Vue.config.productionTip = false;
 
